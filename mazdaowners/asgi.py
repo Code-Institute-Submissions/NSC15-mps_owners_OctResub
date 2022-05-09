f567e0cf71c5b011c44bd11d664e0d33861fe7df
NSC15 <nathancave123@gmail.com>
2022-05-09 20:00:29 +0000

    Initial Deployment Commit

---
+++ mazdaowners/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mazdaowners project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mazdaowners.settings')
+
+application = get_asgi_application()
---
+++ mpsblog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MpsblogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mpsblog'
