ef72b223851d1907d46beb3f86ad2edac6178806
NSC15 <nathancave123@gmail.com>
2022-05-10 18:46:02 +0000

    create a user creation model and register on admin.py

--- mpsblog/models.py
+++ mpsblog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class CreateUser(models.Model):
+    name = models.CharField(max_length=20, help_text='Enter your name')
+    email = models.EmailField(max_length=250)
+    password = models.CharField(max_length=20)
+    city = models.CharField(max_length=30, help_text='Enter your home town location')
+
+    def __str__(self):
+        return self.name
