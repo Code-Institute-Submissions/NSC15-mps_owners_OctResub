65c04d4e8b31966606bbfd88f6028f48220d94c4
NSC15 <nathancave123@gmail.com>
2022-05-12 19:39:44 +0000

    complete post model, add registry to admin.py and create filter by car and post creation to admin site

--- mpsblog/admin.py
+++ mpsblog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
+from django.contrib.admin import ModelAdmin
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(ModelAdmin):
+    list_filter = ('car', 'created_on')
--- mpsblog/models.py
+++ mpsblog/models.py
@@ -4,13 +4,16 @@ from cloudinary.models import CloudinaryField
 
 class Post(models.Model):
     title = models.CharField(max_length=300, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     car = models.CharField(max_length=200, default="Mazda")
     text = models.TextField()
     post_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
     
-
 
     def __str__(self):
         return self.title
+
+    def total_likes(self):
+        return self.likes.count()
