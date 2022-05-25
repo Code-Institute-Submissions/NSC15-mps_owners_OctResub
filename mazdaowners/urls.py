072066e88461c492925bedf7cdf812f2e5557ca7
NSC15 <nathancave123@gmail.com>
2022-05-25 18:13:42 +0000

    download allauth, link up allauth urls, add css to base.html nav links, create user authentification if statements for navbar links

--- mazdaowners/urls.py
+++ mazdaowners/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("mpsblog.urls"), name='mpsblog_urls')
+    path("", include("mpsblog.urls"), name='mpsblog_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
